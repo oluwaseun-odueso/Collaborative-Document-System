a0ba2c6eec97de41b9a5b8d83a5e38498db911cd
oluwaseun-odueso <seunoduez@gmail.com>
2025-04-23 19:00:51 +0100

    chore: used response interceptor in main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ResponseInterceptor } from './interceptors/response.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,8 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  app.useGlobalInterceptors(new ResponseInterceptor())
 
   await app.listen(process.env.PORT ?? 3000);console.log("Server started successfully")
 }
